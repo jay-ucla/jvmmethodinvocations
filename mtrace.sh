9d2e2daf52ae98af8ff2bdfc593895fbc43edc31
jay-ucla <jasa92@g.ucla.edu>
2019-07-11 12:50:53 -0700

    Added LD_LIBRARY_PATH export to set up the path to jmtrace dependency

--- mtrace.sh
+++ mtrace.sh
@@ -1,1 +1,2 @@
+export LD_LIBRARY_PATH=/home/jay/openjdktest/test/jshrink-mtrace/java_crw_jshrink
 java -Xbootclasspath/a:/home/jay/openjdktest/test/jshrink-mtrace/jmtrace/jmtrace.jar -agentpath:/home/jay/openjdktest/test/jshrink-mtrace/jmtrace/libjmtrace.so $1
